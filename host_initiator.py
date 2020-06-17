b7dfc91a169da8d68bc113e9e115dd4db3358d40
Mark <userhei@126.com>
2020-06-17 17:34:11 +0800

    Mark: clean up, need more test

--- host_initiator.py
+++ host_initiator.py
@@ -5,6 +5,7 @@ import re
 import sys
 import time
 
+vplx_ip = '10.203.1.199'
 host = '10.203.1.200'
 port = '22'
 user = 'root'
@@ -20,39 +21,32 @@ class HostTest(object):
         self.id = unique_id
 
     def iscsi_login(self):
-        login_result = self.ssh.excute_command(
-            'iscsiadm -m discovery -t st -p 10.203.1.199 -l')
-        login_result = login_result.decode('utf-8')
+        login_cmd = f'iscsiadm -m discovery -t st -p {vplx_ip} -l'
+        login_result = self.ssh.excute_command(login_cmd)
         if login_result:
+            login_result = login_result.decode('utf-8')
             re_login = re.compile(
-                'Login to.*portal: (10.203.1.199).*successful')
+                f'Login to.*portal: ({vplx_ip}).*successful')
             re_result = re_login.findall(login_result)
             if re_result:
-                print('iscsi login succeed')
                 return True
             else:
-                print('iscsi login failed')
-                sys.exit()
+                s.pe(f'iscsi login to {vplx_ip} failed')
 
     def find_session(self):
-        f_cmd = 'iscsiadm -m session'
-        f_result = self.ssh.excute_command(f_cmd)
-        f_result = f_result.decode('utf-8')
-        if f_result:
-            re_fsession = re.compile('tcp:.*(10.203.1.199):.*')
-            re_result = re_fsession.findall(f_result)
+        session_cmd = 'iscsiadm -m session'
+        session_result = self.ssh.excute_command(session_cmd)
+        if session_result:
+            session_result = session_result.decode('utf-8')
+            re_session = re.compile(f'tcp:.*({vplx_ip}):.*')
+            re_result = re_session.findall(session_result)
             if re_result:
                 return True
-            else:
-                self.iscsi_login()
 
     def _find_device(self, command_result):
-        # re_find_id_dev = re.compile(
-        #     r'\:(\d*)\].*NETAPP[ 0-9a-zA-Z.]*(/dev/sd[a-z]{1,3})')
         re_find_id_dev = re.compile(
             r'\:(\d*)\].*LIO-ORG[ 0-9a-zA-Z._]*(/dev/sd[a-z]{1,3})')
         re_result = re_find_id_dev.findall(command_result)
-        # print(re_result)
 
         # [('0', '/dev/sdb'), ('1', '/dev/sdc')]
         if re_result:
@@ -79,19 +73,13 @@ class HostTest(object):
             print('scan failed')
             sys.exit()
 
-    def find_explore(self):
-        if self.find_session:
-            dev_path = self.explore_disk()
-            return dev_path
-
     def _judge_format(self, arg_bytes):
         re_done = re.compile(r'done')
         string = arg_bytes.decode('utf-8')
         if len(re_done.findall(string)) == 4:
             return True
 
-    def format_mount(self):
-        dev_name = self.find_explore()
+    def format_mount(self,dev_name):
         format_cmd = f'mkfs.ext4 {dev_name} -F'
         cmd_result = self.ssh.excute_command(format_cmd)
         if self._judge_format(cmd_result):
@@ -124,6 +112,22 @@ class HostTest(object):
         if test_result:
             return self._get_dd_perf(test_result)
 
+    def get_test_perf(self):
+        write_perf = self.write_test()
+        print(f'write speed: {write_perf}')
+        time.sleep(0.5)
+        read_perf = self.read_test()
+        print(f'read speed: {read_perf}')
+
+    def start_test(self):
+        if not self.find_session:
+            self.iscsi_login()
+        dev_name = self.explore_disk()
+        mount_status = self.format_mount(dev_name)
+        if mount_status:
+            self.get_test_perf()
+        else:
+            s.pe(f'Device {dev_name} mount failed')
 
 if __name__ == "__main__":
     test = HostTest(21)
