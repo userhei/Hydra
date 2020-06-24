480c4b737077f008702b27e4aa1c4f313a48cb2b
Mark <userhei@126.com>
2020-06-24 16:34:26 +0800

    Mark:add some log msg to vplx.py

--- vplx.py
+++ vplx.py
@@ -32,6 +32,7 @@ class VplxDrbd(object):
         self.res_name = f'res_{unique_name}_{unique_id}'
         self.blk_dev_name = None
         self.drbd_device_name = f'drbd{unique_id}'
+        #log INF:info:start to config drbd resource {r_name}
 
     # def _find_blk_dev(self, id, ls_result):
     #     '''
@@ -50,19 +51,35 @@ class VplxDrbd(object):
         '''
         Scan and find the disk from NetApp
         '''
-        if self.ssh.excute_command('/usr/bin/rescan-scsi-bus.sh'):
+        #log INF:info:start to discover_new_lun for id {self.id}
+        scan_result = self.ssh.excute_command('/usr/bin/rescan-scsi-bus.sh')
+        if scan_result[0] == 1:
+            #log DAT:output:cmd:rescan-scsi-bus.sh:scan_result[1]
             lsscsi_result = self.ssh.excute_command('lsscsi')
+            #log DAT:output:cmd:lsscsi:lsscsi_result
+        # if self.ssh.excute_command('/usr/bin/rescan-scsi-bus.sh'):#新的返回值有状态和数值,以状态判断,记录数值
+        #     lsscsi_result = self.ssh.excute_command('lsscsi')
+            
         else:
             s.pe(f'Scan new LUN failed on NetApp')
+            #log INF:warning:{cmd} execute failed on {host} (类似命令执行失败可以写成一个log类的方法,直接传入参数好了)
         re_find_id_dev = r'\:(\d*)\].*NETAPP[ 0-9a-zA-Z._]*(/dev/sd[a-z]{1,3})'
-        self.blk_dev_name = s.GetDiskPath(self.id, re_find_id_dev, lsscsi_result, 'NetApp').explore_disk()
-        print(f'Find device {self.blk_dev_name} for LUN id {self.id}')
+        #log INF:info:start to config drbd resource {r_name}
+        if lsscsi_result:
+            self.blk_dev_name = s.GetDiskPath(self.id, re_find_id_dev, lsscsi_result, 'NetApp').explore_disk()
+            print(f'Find device {self.blk_dev_name} for LUN id {self.id}')
+            #log DAT:output:xxx(大概要说明一下,re):self.blk_dev_name
+            #log INF:info: new disk device is {self.blk_dev_name}
+        else:
+            s.pe(f'Command "lsscsi" failed on {host}')
+            #log INF:warning:{cmd} execute failed on {host} 
             
 
     def prepare_config_file(self):
         '''
         Prepare DRDB resource config file
         '''
+        #log INF:info:start prepare config fiel for resource {self.res_name}
         context = [rf'resource {self.res_name} {{',
                    rf'\ \ \ \ on maxluntarget {{',
                    rf'\ \ \ \ \ \ \ \ device /dev/{self.drbd_device_name}\;',
@@ -72,7 +89,7 @@ class VplxDrbd(object):
                    rf'\ \ \ \ \ \ \ \ meta-disk internal\;',
                    r'\ \ \ \}',
                    r'}']
-
+        #log DAT:cmd:(这个比较特殊,再讨论)
         # for echo_command in context:
         #     echo_result = self.ssh.excute_command(
         #         f'echo {echo_command} >> /etc/drbd.d/{self.res_name}.res')
@@ -88,31 +105,39 @@ class VplxDrbd(object):
             else:
                 echo_result = self.ssh.excute_command(
                     f'echo {context[i]} >> /etc/drbd.d/{self.res_name}.res')
-            if echo_result is True:
+            #result of ssh command like (1,'done'),1 for status, 'done' for data.
+            if echo_result[0] == 1:
                 continue
             else:
                 s.pe('fail to prepare drbd config file..')
+                #log INF:Warning:drbd config file failed 
         print(f'Create DRBD config file "{self.res_name}.res" done')
+        #log INF:info:successful 
 
     def _drbd_init(self):
         '''
         Initiakize DRBD resource
         '''
+        #log INF:info:start to init drbd for {self.res_name}
         init_cmd = f'drbdadm create-md {self.res_name}'
         drbd_init = self.ssh.excute_command(init_cmd)
-
+        #log DAT:output:cmd:f'{init_cmd}':start to init drbd for {self.res_name}
         if drbd_init:
             drbd_init = drbd_init.decode('utf-8')
             re_drbd = re.compile(
                 'New drbd meta data block successfully created')
             re_init = re_drbd.findall(drbd_init)
+            #log DAT:output:re:'xxx':
             if re_init:
                 print(f'{self.res_name} initialize success')
+                 #log INF:info
                 return True
             else:
+                #log INF:Warnig
                 s.pe(f'drbd resource {self.res_name} initialize failed')
 
         else:
+            #log INF:Warnig
             s.pe(f'drbd resource {self.res_name} initialize failed')
 
     def _drbd_up(self):
